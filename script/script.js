abbf736d1ef48534214008fff11973f79a4e0325
Cherry Grace Medico <133615578+cgmed29@users.noreply.github.com>
2023-09-08 10:02:16 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,7 +3,7 @@ function displaySum() {
   let secondNum = Number(document.getElementById('secondNum').innerHTML)
 
   let total = firstNum + secondNum;
-  document.getElementById("answer").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
+  document.getElementById("result").innerHTML = ` ${firstNum} + ${secondNum}, equals to ${total}` ;
 }
 
 document.getElementById('sumButton').addEventListener("click", displaySum);
