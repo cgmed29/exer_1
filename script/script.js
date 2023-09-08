f05d68700001985331665b6fdae9fade8aa2ecad
Cherry Grace Medico <133615578+cgmed29@users.noreply.github.com>
2023-09-08 10:28:45 +0800

    Update script.js

--- script/script.js
+++ script/script.js
@@ -18,3 +18,28 @@ function displayDifference() {
 
 document.getElementById('diffButton').addEventListener("click", displayDifference);
 
+function displayProduct() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  let product = firstNum * secondNum;
+  document.getElementById("result").innerHTML = ` ${firstNum} * ${secondNum}, equals to ${product}` ;
+}
+
+document.getElementById('prodButton').addEventListener("click", displayProduct);
+
+function displayQuotient() {
+  let firstNum = Number(document.getElementById('firstNum').innerHTML);
+  let secondNum = Number(document.getElementById('secondNum').innerHTML);
+
+  if (secondNum === 0) {
+    document.getElementById("result").innerHTML = "Cannot divide by zero!";
+  } else {
+    let quotient = firstNum / secondNum;
+    document.getElementById("result").innerHTML = ` ${firstNum} ÷ ${secondNum}, equals to ${quotient}`;
+  }
+}
+
+document.getElementById('quotButton').addEventListener("click", displayQuotient);
+
+
